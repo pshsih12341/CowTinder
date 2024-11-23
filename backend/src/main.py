94f84d4f44de00eb9f2ab665dcc4e334ca2c50df
Kin1599 <dkonopliannikov05@mail.ru>
2024-11-23 23:38:21 +0300

    fix path linux

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,9 +2,9 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from ping.router import router as ping_router
-from cows.router import router as cow_router
-from database import Base, engine
+from .ping.router import router as ping_router
+from .cows.router import router as cow_router
+from .database import Base, engine
 from dotenv import load_dotenv
 
 load_dotenv()
