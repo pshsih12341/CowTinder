f6bf10a9aaba0ad2d3452e91a65e3b883f03ca28
Kin1599 <dkonopliannikov05@mail.ru>
2024-11-23 23:07:32 +0300

    fix path

--- backend/src/config.py
+++ backend/src/config.py
@@ -7,7 +7,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
-DB_PORT = os.getenv("DB_PORT")
+DB_PORT = os.getenv("DB_PORT", 5432)
 
 # Проверяем, что все переменные окружения загружены
 if not all([DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME]):
--- backend/src/database.py
+++ backend/src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from .config import DB_USER, DB_PASS, DB_HOST, DB_NAME, DB_PORT
+from config import DB_USER, DB_PASS, DB_HOST, DB_NAME, DB_PORT
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
